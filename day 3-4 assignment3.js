f503191452acd89e5a632e28db5abab5b6905876
rohitbca2011 <69254804+rohitbca2011@users.noreply.github.com>
2020-09-11 14:10:06 +0530

    Add files via upload

---
+++ day 3-4 assignment3.js
@@ -0,0 +1,51 @@
+let player = [
+        {
+        	name:"Virat Kholi",
+        	age: "31",
+        	country: "India",
+        	hobbies:["singing","football","dancing","travelling"]
+        },
+        {
+        	name:"Shane Watson",
+        	age: "36",
+        	country: "austrialia",
+        	hobbies:["Playing guitar", "singing" ]       
+         },
+        {
+        	name:"Chris Gayle",
+        	age: "28",
+        	country: " West Indies",
+        	hobbies:["Dancing, Driving, Listening to Bollywood songs."]
+        },
+        {
+        	name:"Benjamin Andrew Stokes",
+        	age: "40",
+        	country: "England",
+        	hobbies:["Listening music","travellin"]
+        },
+        {
+        	name:"Prithvi Pankaj Shaw ",
+        	age: "20",
+        	country: "India",
+        	hobbies:["Playing on PlayStation , Playing Billiards"]
+        },
+        {
+        	name:"Mahendra Singh Dhoni",
+        	age: "39",
+        	country: "India",
+        	hobbies:["Biking, Playing Football and Tennis, Swimming"]
+        },
+        {
+        	name:"Rohit Gurunath Sharma",
+        	age: "33",
+        	country: "India",
+        	hobbies:[" Travelling, watching movies, playing table tennis "]
+        }
+        ];
+        function display(){
+        	for (let i = 0; i <player.length; i++) {
+        	console.log (player[i]);
+        	}
+          
+        }
+        display();
